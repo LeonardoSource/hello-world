5fcc38bab9b8ac22114dc3d70d95f8ca89043b16
LeonardoSource <58079241+LeonardoSource@users.noreply.github.com>
2020-02-21 10:03:06 -0300

    A simple hello

---
+++ simplehello.py
@@ -0,0 +1,15 @@
+print('''
+====================================
+H   H HHHHH H    H    HHHHH
+H   H H     H    H    H   H
+HHHHH HHHH  H    H    H   H
+H   H H     H    H    H   H
+H   H HHHHH HHHH HHHH HHHHH
+
+]]]    ]]]]] ]      ] .....
+]  ]]  ]     ]      ] .
+]    ] ]]]]   ]    ]  .....
+]  ]]  ]       ]  ]       .
+]]]    ]]]]]    ]]    .....
+'''
+)
